01636fd9653a63cd727acb82024ac81208c51400
kevinbyte77 <kevinspicy74@gmail.com>
2026-07-27 18:33:39 +0530

    Delete WrappeClassDemo.java

--- WrappeClassDemo.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner
-public class WrappeClassDemo 
-{
-    Scanner sc=new Scanner(System.in);
-
-    System.out.print("Enter first number:");
-    String str1=sc.newLine();
-
-}
