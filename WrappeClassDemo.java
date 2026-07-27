53513c31e655532c474c38654e4d7dc06f27599f
kevinbyte77 <kevinspicy74@gmail.com>
2026-07-27 18:33:03 +0530

    Add files via upload

---
+++ WrappeClassDemo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner
+public class WrappeClassDemo 
+{
+    Scanner sc=new Scanner(System.in);
+
+    System.out.print("Enter first number:");
+    String str1=sc.newLine();
+
+}
